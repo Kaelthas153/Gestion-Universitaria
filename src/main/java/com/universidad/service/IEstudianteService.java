d716f00883abd8cf62b5171f777d5d73ae580d8f
Kaelthas153 <kaeltas.cuevas@gmail.com>
2025-03-30 15:03:02 -0400

    Practica 1

--- src/main/java/com/universidad/service/IEstudianteService.java
+++ src/main/java/com/universidad/service/IEstudianteService.java
@@ -7,5 +7,9 @@ import java.util.List; // Importa la interfaz List para manejar listas
 public interface IEstudianteService { // Define la interfaz IEstudianteService
     
     List<EstudianteDTO> obtenerTodosLosEstudiantes(); // Método para obtener una lista de todos los EstudianteDTO
-    
+    EstudianteDTO obtenerEstudiantePorId(Long id); // Nuevo método para obtener un estudiante por su ID
+
+    EstudianteDTO actualizarEstudiante(Long id, EstudianteDTO estudianteDTO); // Nuevo método para actualizar un estudiante
+    EstudianteDTO registrarEstudiante(EstudianteDTO estudianteDTO);
+    void eliminarEstudiante(Long id);
 }
